2b4b0390669e4e4afe2683c46b922aaff7f91c2a
erickfirmo <erickluques@gmail.com>
2020-10-07 18:08:38 -0300

    Change component state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import React from 'react';
 class App extends React.Component {
 
   state = {
-    name : 'Érick Firmo'
+    name: ''
+  }
+
+  changeName = (event) => {
+    let name = event.target.value;
+    this.setState({
+      name: name
+    });
   }
 
   render() {
     return (
-      <>
-        {this.state.name}
-      </>
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.changeName}></input>
+        <h1>Hello {this.state.name}</h1>
+      </React.Fragment>
     )
   }
 }
