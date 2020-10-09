6fa3be359f6f53954c5a507c6e585941a89a0fce
erickfirmo <erickluques@gmail.com>
2020-10-09 16:45:53 -0300

    Adicionando state "date" no componente Clock

--- src/Clock.js
+++ src/Clock.js
@@ -3,6 +3,7 @@ import React from 'react';
 class Clock extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {date: new Date()};
     }
 
     componentDidMount() {
