1d3086c22c89f758305ad1dc7f3cf71f1c7f943b
erickfirmo <erickluques@gmail.com>
2020-10-10 17:53:58 -0300

    Importando index.css em index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
     <App 
       name="Érick Firmo"
       age={24}
-    />
-  </React.StrictMode>,
+    />,
   document.getElementById('root')
 );
