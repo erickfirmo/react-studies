0581bb02e904b5e8441af438763594599652bc82
erickfirmo <erickluques@gmail.com>
2020-10-16 18:20:54 -0300

    Atualizando App.js

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
+import Navbar from './components/navbar'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
