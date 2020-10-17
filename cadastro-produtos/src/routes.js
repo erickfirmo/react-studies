3ccdbb7da089745d22ac2acebe90d2020240fb67
erickfirmo <erickluques@gmail.com>
2020-10-16 22:40:45 -0300

    Criando rota de cadastro de produtos

--- cadastro-produtos/src/routes.js
+++ cadastro-produtos/src/routes.js
@@ -1,3 +1,16 @@
 import React from 'react'
 
 import { HashRouter, Switch, Route } from 'react-from-dom'
+
+import Home from './views/home'
+import CreateProduct from './views/products/create'
+
+export default () => {
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/cadastro-produtos" component={CreateProduct} />
+            </Switch>
+        </HashRouter>
+    )
+}
