63cacb47b31c67d588cb8b0d4ada482cc0c4c957
erickfirmo <erickluques@gmail.com>
2020-10-26 20:27:20 -0300

    Atualizando componente do formulario de produtos

--- cadastro-produtos/src/views/products/create.js
+++ cadastro-produtos/src/views/products/create.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
+import ProductService from '../../app/ProductService';
+
 class CreateProduct extends React.Component {
+
+    constructor() {
+        super()
+        this.service = new ProductService()
+    }
 
     state = {
         name: '',
@@ -9,7 +16,35 @@ class CreateProduct extends React.Component {
         price: 0,
         provider: '',
     }
-    
+
+    onChange = (event) => {
+        const value = event.target.value
+        const fieldName = event.target.name
+        this.setState({ [fieldName]: value })
+    }
+
+    onSubmit = (event) => {
+        const product = {
+            name: this.state.name,
+            sku: this.state.sku,
+            description: this.state.description,
+            price: this.state.price,
+            provider: this.state.provider,
+        }
+
+        this.service.save()
+    }
+
+    clearFields = () => {
+        this.setState({
+            name: '',
+            sku: '',
+            description: '',
+            price: 0,
+            provider: '',
+        })
+    }
+
     render() {
         return (
             <div className="card">
@@ -21,13 +56,21 @@ class CreateProduct extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Nome: *</label>
-                                <input type="text" value={this.state.name} className="form-control" />
+                                <input type="text"
+                                    name="name"
+                                    value={this.state.name}
+                                    className="form-control"
+                                    onChange={this.onChange} />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>SKU: *</label>
-                                <input type="text" value={this.state.sku} className="form-control" />
+                                <input className="form-control"
+                                       type="text"
+                                       name="sku"
+                                       value={this.state.sku}
+                                       onChange={this.onChange} />
                             </div>
                         </div>
                     </div>
@@ -36,7 +79,11 @@ class CreateProduct extends React.Component {
                         <div className="col-md-12">
                             <div className="form-group">
                                 <label>Descrição:</label>
-                                <textarea className="form-control" value={this.state.description}></textarea>
+                                <textarea name="description"
+                                          value={this.state.description}
+                                          className="form-control"
+                                          onChange={this.onChange}>        
+                                </textarea>
                             </div>
                         </div>
                     </div>
@@ -45,23 +92,31 @@ class CreateProduct extends React.Component {
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Preço: *</label>
-                                <input type="text" value={this.state.price} className="form-control" />
+                                <input type="text"
+                                       name="price"
+                                       value={this.state.price}
+                                       className="form-control" 
+                                       onChange={this.onChange} />
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="form-group">
                                 <label>Fornecedor: *</label>
-                                <input type="text" value={this.state.provider} className="form-control" />
+                                <input type="text"
+                                       name="provider"
+                                       value={this.state.provider}
+                                       className="form-control"
+                                       onChange={this.onChange} />
                             </div>
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="col-md-1">
-                            <button className="btn btn-success">Salvar</button>
+                            <button onClick={this.onSubmit} className="btn btn-success">Salvar</button>
                         </div>
                         <div className="col-md-1">
-                            <button className="btn btn-primary">Limpar</button>
+                            <button onClick={this.clearFields} className="btn btn-primary">Limpar</button>
                         </div>
                     </div>
                 </div>
