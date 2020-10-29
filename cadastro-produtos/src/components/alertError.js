7a0d4de3aaa0a1f884a938725174366a3867bcc2
erickfirmo <erickluques@gmail.com>
2020-10-28 22:00:11 -0300

    Criando metodo close no componente AlertError.js

--- cadastro-produtos/src/components/alertError.js
+++ cadastro-produtos/src/components/alertError.js
@@ -4,16 +4,31 @@ class AlertError extends React.Component {
 
     constructor(props){
         super(props)
+        this.setState({ showComponent: true })
+    }
+    
+    state = {
+        showComponent: true
     }
 
-    render() {
-        return (
-            <div className="alert alert-dismissible alert-danger" key={this.props.key}>
-                <button type="button" className="close" data-dismiss="alert">&times;</button>
-                <strong>Erro!</strong> {this.props.message}
-                <a href="#" className="alert-link"></a>
-            </div>
-        )
+    close = () => {
+        this.setState({ showComponent: false })
+    }
+
+    render() {         
+        if (this.state.showComponent == true) {
+
+            return (
+                <div className="alert alert-dismissible alert-danger" key={this.props.key}>
+                    <button type="button" className="close" data-dismiss="alert" onClick={ this.close }>&times;</button>
+                    <strong>Erro!</strong> {this.props.message}
+                    
+                    <a href="#" className="alert-link"></a>
+                </div>
+            )
+        } else {
+            return <></>
+        }
     }
 
 }
