e469e06a0e9cadf1b41a39296890ef67b36851e6
erickfirmo <erickluques@gmail.com>
2020-11-01 00:42:23 -0300

    Criando componente de listagem de produtos

--- cadastro-produtos/src/routes.js
+++ cadastro-produtos/src/routes.js
@@ -4,12 +4,14 @@ import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import Home from './views/home'
 import CreateProduct from './views/products/create'
+import ListProduct from './views/products/list'
 
 export default () => {
     return (
         <HashRouter>
             <Switch>
                 <Route exact path="/cadastro-produtos" component={CreateProduct} />
+                <Route exact path="/consulta-produtos" component={ListProduct} />
                 <Route exact path="/" component={Home} />
             </Switch>
         </HashRouter>
